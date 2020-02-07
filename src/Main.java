6622914bcd570cb6875415ccb53ea1b159834c0f
kristynf <thetruthandsoul@gmail.com>
2020-02-07 12:25:47 -0500

    updated:

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,60 @@
+import com.sun.deploy.security.SelectableSecurityManager;
+
+import java.util.Scanner;
+
 public class Main {
-    public static void main (String[]args){
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ResumeDB a = new ResumeDB();
+        Resume b = new Resume();
+
+
+        a.welcome();
+
+        System.out.println("What is your first and last name? ");
+        b.setName(sc.nextLine());
+        System.out.println("What is your email address? ");
+        b.setEmailAddress(sc.nextLine());
+        System.out.println("Where was your degree obtained from? ");
+        b.setUniversityName(sc.nextLine());
+        System.out.println("What type of degree was obtained? ");
+        b.setDegreeType(sc.nextLine());
+        System.out.println("What did you major in? ");
+        b.setMajor(sc.nextLine());
+        System.out.println("What year did you graduate? ");
+        b.setGradYear(sc.nextInt());
+        sc.nextLine();
+        System.out.println("What company did you work for? ");
+        b.setCompany(sc.nextLine());
+        System.out.println("What was your job title? ");
+        b.setJobTitle(sc.nextLine());
+        System.out.println("What was your start date MM/DD/YYYY? ");
+        b.setStartDate(sc.nextLine());
+        System.out.println("What was your end date MM/DD/YYYY? ");
+        b.setEndDate(sc.nextLine());
+        System.out.println("Describe your job duties: ");
+        b.setDescription(sc.nextLine());
+        for (int i = 0; i < 3; i++) {
+            if (i < 3) {
+                System.out.println("Enter your skill: ");
+                b.setSkillName(sc.nextLine());
+                System.out.println("Enter your proficiency at your skill: ");
+                b.setSkillProficiency(sc.nextLine());
+
+            } else {
+                break;
+            }
+
+        }
+
+
+        System.out.println("Please enter your name to retrieve your spiffy resume: ");
+             String input = sc.nextLine();
+             ResumeDB database = new ResumeDB(input);
+
+
+
 
     }
 }
--- src/Resume.java
+++ src/Resume.java
@@ -1,17 +1,39 @@
 public class Resume {
     private String name;
     private String emailAddress;
-
-    public Resume(){
-
+    private String degreeType;
+    private String universityName;
+    private String major;
+    private int gradYear;
+    private String company;
+    private String jobTitle;
+    private String startDate;
+    private String endDate;
+    private String description;
+    private String skillName;
+    private String skillProficiency;
+//default constructor
+    public Resume() {
+//overloaded constructor
     }
 
-    public Resume(String name, String emailAddress){
-        this.name=name;
-        this.emailAddress=emailAddress;
-
+    public Resume(String name, String emailAddress, String degreeType, String universityName, String major, int gradYear, String company,
+                  String jobTitle, String startDate, String endDate, String description, String skillName, String skillProficiency) {
+        this.name = name;
+        this.emailAddress = emailAddress;
+        this.degreeType = degreeType;
+        this.universityName = universityName;
+        this.major = major;
+        this.gradYear = gradYear;
+        this.company = company;
+        this.jobTitle = jobTitle;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.description = description;
+        this.skillName = skillName;
+        this.skillProficiency = skillProficiency;
     }
-
+//getters and setters
     public String getName() {
         return name;
     }
@@ -27,10 +49,120 @@ public class Resume {
     public void setEmailAddress(String emailAddress) {
         this.emailAddress = emailAddress;
     }
+
+    public String getDegreeType() {
+        return degreeType;
+    }
+
+    public void setDegreeType(String degreeType) {
+        this.degreeType = degreeType;
+    }
+
+    public String getUniversityName() {
+        return universityName;
+    }
+
+    public void setUniversityName(String universityName) {
+        this.universityName = universityName;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+
+    public int getGradYear() {
+        return gradYear;
+    }
+
+    public void setGradYear(int gradYear) {
+        this.gradYear = gradYear;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getSkillName() {
+        return skillName;
+    }
+
+    public void setSkillName(String skillName) {
+        this.skillName = skillName;
+    }
+
+    public String getSkillProficiency() {
+        return skillProficiency;
+    }
+
+    public void setSkillProficiency(String skillProficiency) {
+        this.skillProficiency = skillProficiency;
+    }
+//code to display the resume (hope it works!)
+    public String getDisplayResume() {
+        return "++--++--++--++--++--++--++--++--++--++--++--++--++--++--++--++"
+                + getName() + "\n"
+                + getEmailAddress() + "\n " +
+                "\n" +
+                "EDUCATION" +
+                "\n"
+                + getDegreeType() + " in " + getMajor() +
+                "\n"
+                + getUniversityName() + ", " + getGradYear() +
+                "\n"
+                + "WORK EXPERIENCE" +
+                "\n"
+                + getJobTitle() +
+                "\n"
+                + getCompany() + ", " + getStartDate() + " - " + getEndDate() + "\n"
+                + "       " + getDescription() + "\n"
+                + "SKILLS" + "\n"
+                + getSkillName() + ", " + getSkillProficiency() + "\n"
+                + getSkillName() + ", " + getSkillProficiency() + "\n"
+                + getSkillName() + ", " + getSkillProficiency() + "\n";
+
+
+    }
+
 }
 
 
-
-
-    /*a name,
-    an email address, */
